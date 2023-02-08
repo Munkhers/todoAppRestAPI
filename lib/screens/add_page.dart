2075798aa9c93b47b61594cabd82cbf9162362a9
smunkherdene <muug@icodice.com>
2023-02-08 17:50:03 -0600

    first commit

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+
+class AddTodoPage extends StatefulWidget {
+  final Map? todo;
+  const AddTodoPage({super.key, this.todo});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+
+  @override
+  void initState() {
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo['title'];
+      final description = todo['description'];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(isEdit ? 'Edit Todo' : 'Add Todo'),
+      ),
+      body: ListView(children: [
+        TextField(
+          controller: titleController,
+          decoration: const InputDecoration(hintText: 'Title'),
+        ),
+        const SizedBox(height: 30),
+        TextField(
+          controller: descriptionController,
+          decoration: const InputDecoration(hintText: 'Description'),
+          keyboardType: TextInputType.multiline,
+          minLines: 5,
+          maxLines: 8,
+        ),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {
+            submitData();
+          },
+          child: Text(isEdit ? 'Update' : 'Submit'),
+        )
+      ]),
+    );
+  }
+
+  Future<void> updateData() async {
+    // Update tooooooo dooooooooo
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+    final response =
+        await Dio().post('http://api.nstack.in/v1/todos', data: body);
+    if (response.statusCode == 201) {
+      titleController.text = '';
+      descriptionController.text = '';
+      showSuccessMessage('success');
+    } else {
+      showErrorMessage('error');
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+}
+
+//Todo App using Rest API
---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,110 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/screens/add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  var items = [];
+  bool isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
+  Future<void> fetchTodo() async {
+    var response =
+        await Dio().get('http://api.nstack.in/v1/todos?page=1&limit=10');
+    if (response.statusCode == 200) {
+      setState(() {
+        items = response.data['items'];
+      });
+    }
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo list'),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: ((context, index) {
+              final item = items[index] as Map;
+              final id = item['_id'] as String;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}')),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+                trailing: PopupMenuButton(
+                  onSelected: (value) {
+                    if (value == 'edit') {
+                      navigateToEditPage(item);
+                    } else if (value == 'delete') {
+                      deleteById(id);
+                    }
+                  },
+                  itemBuilder: (context) {
+                    return const [
+                      PopupMenuItem(value: 'edit', child: Text('Edit')),
+                      PopupMenuItem(
+                        value: 'delete',
+                        child: Text('Delete'),
+                      ),
+                    ];
+                  },
+                ),
+              );
+            }),
+          ),
+        ),
+        child: const Center(child: CircularProgressIndicator()),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAdd,
+        label: const Text('Add Todo'),
+      ),
+    );
+  }
+
+  Future<void> navigateToAdd() async {
+    final route = MaterialPageRoute(
+      builder: ((context) => const AddTodoPage()),
+    );
+    await Navigator.push(context, route);
+    setState(() {
+      isLoading = true;
+    });
+    fetchTodo();
+  }
+
+  void navigateToEditPage(Map item) {
+    final route = MaterialPageRoute(
+      builder: ((context) => AddTodoPage(todo: item)),
+    );
+    Navigator.push(context, route);
+  }
+
+  Future<void> deleteById(String id) async {
+    final response = await Dio().delete('https://api.nstack.in/v1/todos/$id');
+    if (response.statusCode == 200) {
+      setState(() {
+        items = items.where((element) => element['_id'] != id).toList();
+      });
+    }
+  }
+}
